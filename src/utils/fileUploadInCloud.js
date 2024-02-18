66471e7952cf3a073a6276e7767600839031bef2
Rajdeep <rajdeep98543231chowdhury@gmail.com>
2024-02-18 11:16:36 +0530

    user controller and bug fixes

--- src/utils/fileUploadInCloud.js
+++ src/utils/fileUploadInCloud.js
@@ -3,8 +3,8 @@ import fs from "fs"
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -18,7 +18,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
         // file has been uploaded successful 
         console.log("file has been uploaded successfully +" + response.url);
-        fs.unlinkSync(localFilePath)
+        //fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)
@@ -26,5 +26,5 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary }
 
